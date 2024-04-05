eb69cd0649d32656749b9378dbf22939bbbac6dc
roar605 <rksingh6050@gmail.com>
2024-04-05 19:52:18 +0530

    file upload controller

--- controllers/File.controller.js
+++ controllers/File.controller.js
@@ -2,3 +2,22 @@ const File = require("../models/File.model.js");
 
 // localfile->handler function
 
+exports.localFileUpload = async (req, res) => {
+    try {
+        //fetch file
+        const file = req.files.file;
+        console.log("File is here : ", file);
+
+        let path = __dirname + "/files/" + Date.now();
+        console.log("Path of file : ", path);
+
+
+        file.mv(path, (err) => { console.log(err) });
+        res.json({
+            success: true,
+            message: "Local file uploaded successfully"
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
