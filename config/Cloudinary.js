f706549a8ff556b1ba35a070de1a5187acfa0313
roar605 <rksingh6050@gmail.com>
2024-04-08 03:08:35 +0530

    saved

--- config/Cloudinary.js
+++ config/Cloudinary.js
@@ -1,15 +1,14 @@
 const cloudinary = require("cloudinary");
-const { ApiError } = require("../utils/ApiError");
 
 
-exports.cloudinaryConnect = ()=>{
+exports.cloudinaryConnect = () => {
     try {
         cloudinary.config({
-            cloud_name:process.env.CLOUD_NAME,
-            api_key:process.env.API_KEY,
-            api_secret:process.env.API_SECRET
+            cloud_name: process.env.CLOUD_NAME,
+            api_key: process.env.API_KEY,
+            api_secret: process.env.API_SECRET
         })
     } catch (error) {
-        throw new ApiError(400,"Cloudinary connection failed",error);
+        console.log(error);
     }
 }
--- config/Database.js
+++ config/Database.js
@@ -1,14 +1,12 @@
-import {ApiError} from "../utils/ApiError.js";
+
 const mongoose =require("mongoose");
 require("dotenv").config();
 
 exports.connect = ()=>{
-    mongoose.connect(process.env.MONGO_DB_URL,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true
+    mongoose.connect(process.env.MONGO_DB_URL,{        
     })
     .then(console.log("DB connection successfull"))
     .catch((error)=>{
-        throw new ApiError(500,"DB connection issue",error)
+        console.log("DB Connection : ",error);
     })
 }
